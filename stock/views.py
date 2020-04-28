00dc78dc565bea8b6ed757dbeec7f88d8f7c5217
Amnuay Pintong <amnuay.p@otiknetwork.com>
2020-04-28 12:01:11 +0700

    test api

--- stock/views.py
+++ stock/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from suds.client import Client
+import xmltodict, json
 
 #from django.http import HttpResponse
 
@@ -13,7 +15,37 @@ def index(request):
     return render(request,'frontend/index.html',{'products':products})
 
 def about(request):
-    return render(request,'frontend/about.html')
+    
+    tags = ['น้ำตก','ธรรมชาติ','หน้าฝน','ตากหมอก','คอมพิวเตอร์']
+    data = {}
+    
+    data['nature'] = {
+            'name':'บทความท่องเที่ยวภาคเหนือ',
+            'author':'อำนวย ปิ่นทอง',
+            'tags': tags
+            }
+    
+    data['computer'] = {
+            'name':'Django for Beginner..',
+            'author':'John Doe'            
+            }
+        
+    return render(request,'frontend/about.html',data)
 
 def contact(request):
+    
+    client = Client('https://www.pttor.com/OilPrice.asmx?WSDL')
+    OilPrice = client.service.CurrentOilPrice(Language='thai')
+    #หากต้องการใช้ GetOilPrice ให้แก้เป็น client.service.GetOilPrice(รายละเอียดตามเอกสาร)
+    #print(OilPrice)
+    #print(type(OilPrice))
+    OilPrice1 = xmltodict.parse(OilPrice) # ได้ผลลัพธ์เป็น json ในสตริง
+    Prices = eval(json.dumps(OilPrice1)) # เรียกใช้งาน json ในสตริง
+    #print(Prices)
+
+    
+    for v in Prices['PTTOR_DS'].items():
+        for i in v:
+            print(type(i))
+    
     return render(request,'frontend/contact.html')
